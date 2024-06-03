ab3fb38fd2493bb66e2c11fa4dbc4953ec7c1adb
Kevin Lucas Simon <k.simon@basecom.de>
2024-06-03 13:56:07 +0200

    capsule game provider in own game component

---
+++ src/components/Game.tsx
@@ -0,0 +1,51 @@
+import {Canvas} from "@react-three/fiber";
+import {GizmoHelper, GizmoViewport, KeyboardControls, Stats} from "@react-three/drei";
+import {PlayerPhysics} from "./PlayerPhysics";
+import {UserControls} from "./UserControls";
+import {ReactNode, useMemo} from "react";
+
+const Controls = {
+    top: 'top',
+    bottom: 'bottom',
+    left: 'left',
+    right: 'right',
+    jump: 'jump',
+}
+
+export type GameProps = {
+    children?: ReactNode | undefined,
+}
+
+export function Game(props: GameProps) {
+    const keyboardControls = useMemo(()=>[
+        { name: Controls.top, keys: ['ArrowUp'] },
+        { name: Controls.bottom, keys: ['ArrowDown'] },
+        { name: Controls.left, keys: ['ArrowLeft'] },
+        { name: Controls.right, keys: ['ArrowRight'] },
+        { name: Controls.jump, keys: ['Space'] },
+    ], [])
+
+    return (
+        <UserControls>
+            <Canvas>
+                <ambientLight intensity={Math.PI / 2} />
+                <pointLight position={[10, 10, 10]} decay={0} intensity={Math.PI/2} />
+                <pointLight position={[10, -10, -10]} decay={0} intensity={Math.PI/4} />
+                <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI/4} />
+
+                <KeyboardControls map={keyboardControls}>
+                    <PlayerPhysics>
+                        {props.children}
+                    </PlayerPhysics>
+                </KeyboardControls>
+
+                {/*<Grid infiniteGrid={true}/>*/}
+                <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
+                    <GizmoViewport axisColors={['#9d4b4b', '#2f7f4f', '#3b5b9d']} labelColor="white" />
+                </GizmoHelper>
+
+                <Stats />
+            </Canvas>
+        </UserControls>
+    );
+}
