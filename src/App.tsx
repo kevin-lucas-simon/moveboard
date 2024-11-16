d2c0483c247442d5f4a9f861991dbda58c102bd8
Kevin Lucas Simon <k.simon@basecom.de>
2024-11-16 02:11:41 +0100

    moved level from app to game component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import {Game} from "./components/Game";
-import {Level} from "./components/chunks/Level";
 
 export function App() {
     return (
-        <Game>
-            <Level startChunk={"FirstChunk"} />
-        </Game>
+        <Game startChunk={"FirstChunk"}/>
     );
 }
