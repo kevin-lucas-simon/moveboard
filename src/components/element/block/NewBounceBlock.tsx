1617c6fcad3a7f1fa35b1c3d8ca474b39f5dbece
Kevin Lucas Simon <k.simon@basecom.de>
2024-12-21 13:00:25 +0100

    apply player bounce logic

--- src/components/element/block/NewBounceBlock.tsx
+++ src/components/element/block/NewBounceBlock.tsx
@@ -1,6 +1,13 @@
-import {RigidBody} from "@react-three/rapier";
+import {CollisionEnterPayload, RigidBody} from "@react-three/rapier";
 import {NewElementModel} from "../NewElementModel";
 import {Vector3} from "three";
+import {useMemo, useState} from "react";
+import {useFrame} from "@react-three/fiber";
+import {Player} from "../../entities/Player";
+
+// constants for animation finetuning
+const INTENSITY_PHYSIC_FACTOR = 10
+const INTENSITY_ANIMATION_FACTOR = 1
 
 export type NewBounceBlockProps = NewElementModel & {
     diameter: number,
@@ -9,13 +16,59 @@ export type NewBounceBlockProps = NewElementModel & {
 
 /**
  * Bouncer block that bounces the player on collision
- * @param props
+ * @param props.position position of the bouncer
  * @param props.diameter diameter of the bouncer
  * @param props.intensity expected intensity of the bounce
  * @constructor
  */
 export function NewBounceBlock(props: NewBounceBlockProps) {
-    // TODO implement logic for bouncing (see BouncerBlock.tsx)
+    const [bounceAnimation, setBounceAnimation] = useState(0)
+
+    // calculate bounce animation diameter related to the intensity and diameter
+    const bounceAnimationDiameter = useMemo(() => {
+        // to create a realistic animation, the intensity is divided by the sphere surface area (PI is ignored for simplicity
+        const radiusIntensity = props.intensity / Math.pow(props.diameter, 2) * INTENSITY_ANIMATION_FACTOR
+        // calculation of the animation by adding the intensity in virtual spherical volumes of 1 (PI ignored too)
+        return Math.sqrt(Math.pow(props.diameter, 2) * (1 + radiusIntensity))
+    }, [props.diameter, props.intensity])
+
+    // run animation if active
+    useFrame((state, delta) => {
+        if (bounceAnimation > 0) {
+            setBounceAnimation(Math.max(0, bounceAnimation - delta * 5))
+        }
+    })
+
+    // collision event handler
+    function onCollision(event: CollisionEnterPayload) {
+        // is the intersecting object our player?
+        if (event.other.rigidBodyObject?.name !== Player.name) {
+            return
+        }
+
+        // get player and bouncer position
+        const playerPosition = event.other.rigidBodyObject.position
+        const bouncerPosition = event.target.rigidBodyObject?.position
+
+        // calculate bounce direction vector
+        if(!playerPosition || !bouncerPosition) {
+            return;
+        }
+        const bounceDirectionVector = playerPosition.clone()
+            .sub(bouncerPosition)
+            .normalize()
+            .multiplyScalar(props.intensity * INTENSITY_PHYSIC_FACTOR)
+
+        // apply impulse to player
+        const playerRigidBody = event.other.rigidBody
+        if (!playerRigidBody) {
+            return
+        }
+        playerRigidBody.applyImpulse(bounceDirectionVector, true)
+
+        // set animation
+        setBounceAnimation(1)
+    }
 
     return (
         <RigidBody
@@ -23,11 +76,12 @@ export function NewBounceBlock(props: NewBounceBlockProps) {
             position={new Vector3().copy(props.position)}
             type={"fixed"}
             colliders={"ball"}
-            // onCollisionEnter={onCollision}
+            onCollisionEnter={onCollision}
         >
             <mesh>
-                <sphereGeometry args={[props.diameter/2]}/>
-                <meshStandardMaterial color={"grey"} />
+                <sphereGeometry
+                    args={[props.diameter / 2 + bounceAnimation * (bounceAnimationDiameter - props.diameter) / 2]}/>
+                <meshStandardMaterial color={"grey"}/>
             </mesh>
         </RigidBody>
     );
