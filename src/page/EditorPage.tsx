7c5f8de08bc268f16cbc63b6209db9d8d0ec79bd
Kevin Lucas Simon <k.simon@basecom.de>
2024-12-21 14:21:14 +0100

    change GamePage to new game level logic

--- src/page/EditorPage.tsx
+++ src/page/EditorPage.tsx
@@ -1,6 +1,5 @@
 import {NewLevel} from "../components/world/NewLevel";
 import {Experience} from "../components/Experience";
-import {UserControls} from "../components/UserControls";
 import {useLevelDownloader} from "../components/world/hook/useLevelDownloader";
 
 export function EditorPage() {
@@ -8,11 +7,8 @@ export function EditorPage() {
         = useLevelDownloader("TestLevel");
 
     return (
-        <UserControls>
-            {/* TODO Übergreifendes Menü mit Permission-Abfrage -> dann erst <UserControls> einblenden! */}
-            <Experience>
-                {downloadedLevel && <NewLevel {...downloadedLevel} />}
-            </Experience>
-        </UserControls>
+        <Experience>
+            {downloadedLevel && <NewLevel {...downloadedLevel} />}
+        </Experience>
     );
 }
--- src/page/GamePage.tsx
+++ src/page/GamePage.tsx
@@ -1,16 +1,20 @@
 import {UserControls} from "../components/UserControls";
 import {Experience} from "../components/Experience";
-import {Level} from "../components/chunks/Level";
+import {useLevelDownloader} from "../components/world/hook/useLevelDownloader";
+import {NewLevel} from "../components/world/NewLevel";
 
 /**
  * Game page that initializes the game
  */
 export function GamePage() {
+    const downloadedLevel
+        = useLevelDownloader("TestLevel");
+
     return (
         <UserControls>
             {/* TODO Übergreifendes Menü mit Permission-Abfrage -> dann erst <UserControls> einblenden! */}
             <Experience>
-                <Level startChunk={"FirstChunk"} />
+                {downloadedLevel && <NewLevel {...downloadedLevel} />}
             </Experience>
         </UserControls>
     );
