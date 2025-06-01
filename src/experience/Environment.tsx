dbf36b4e20ecc0cc56f7de4d8c8942a9dc22f906
Kevin Lucas Simon <k.simon@basecom.de>
2025-06-01 20:37:10 +0200

    Add suspense

--- src/experience/Environment.tsx
+++ src/experience/Environment.tsx
@@ -3,7 +3,7 @@ import {GizmoHelper, GizmoViewport, Stats} from "@react-three/drei";
 import {useDebugSettings} from "./input/DebugSettingsProvider";
 import {KeyboardKeysProvider} from "./input/KeyboardKeysProvider";
 import {DeviceMotionProvider} from "./input/DeviceMotionProvider";
-import React from "react";
+import React, {Suspense} from "react";
 import {EnvironmentPhysics} from "./world/physic/EnvironmentPhysics";
 
 export type EnvironmentProps = {
@@ -28,23 +28,25 @@ export function Environment(props: EnvironmentProps) {
                     resize={{ debounce: 0 }}
                     className={props.className}
                 >
-                    <ambientLight intensity={Math.PI / 2} />
-                    <pointLight position={[10, 10, 10]} decay={0} intensity={Math.PI/2} />
-                    <pointLight position={[10, -10, -10]} decay={0} intensity={Math.PI/4} />
-                    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI/4} />
+                    <Suspense fallback={null}>
+                        <ambientLight intensity={Math.PI / 2} />
+                        <pointLight position={[10, 10, 10]} decay={0} intensity={Math.PI/2} />
+                        <pointLight position={[10, -10, -10]} decay={0} intensity={Math.PI/4} />
+                        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI/4} />
 
-                    <EnvironmentPhysics>
-                        {props.children}
-                    </EnvironmentPhysics>
+                        <EnvironmentPhysics>
+                            {props.children}
+                        </EnvironmentPhysics>
 
-                    {debug.displayEditorFeatures &&
-                        <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
-                            <GizmoViewport axisColors={['#9d4b4b', '#2f7f4f', '#3b5b9d']} labelColor="white" />
-                        </GizmoHelper>
-                    }
-                    {debug.displayPerformanceStats &&
-                        <Stats />
-                    }
+                        {debug.displayEditorFeatures &&
+                            <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
+                                <GizmoViewport axisColors={['#9d4b4b', '#2f7f4f', '#3b5b9d']} labelColor="white" />
+                            </GizmoHelper>
+                        }
+                        {debug.displayPerformanceStats &&
+                            <Stats />
+                        }
+                    </Suspense>
                 </Canvas>
             </KeyboardKeysProvider>
         </DeviceMotionProvider>
