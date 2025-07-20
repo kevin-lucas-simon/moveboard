d0e89d5ddeb87a0c45a8215282b392ac7456fcc2
Kevin Lucas Simon <k.simon@basecom.de>
2025-07-20 19:42:49 +0200

    Add mouse event selection click

--- src/experience/world/Element.tsx
+++ src/experience/world/Element.tsx
@@ -3,14 +3,35 @@ import {ElementModel} from "../../data/model/element/ElementModel";
 import {elementConfig, elementFallbackConfig} from "../../config/elementConfig";
 import {ElementType} from "../../data/model/element/ElementType";
 import React from "react";
-import {useEditorContext} from "../../editor/reducer/EditorProvider";
+import {useEditorActions, useEditorContext} from "../../editor/reducer/EditorProvider";
+import {ThreeEvent} from "@react-three/fiber/dist/declarations/src/core/events";
 
 export type ElementProps = ElementModel;
 
 export function Element(props: ElementProps) {
     const editorContext = useEditorContext();
+    const editorActions = useEditorActions();
 
     const selected = editorContext?.selected.includes(props.id) ?? false;
+
+    const selectElement = (e: ThreeEvent<MouseEvent>) => {
+        if (!editorContext || !editorActions) {
+            return;
+        }
+        e.stopPropagation();
+
+        if (selected) {
+            editorActions({
+                type: 'editor_deselect_all',
+            });
+            return;
+        } else {
+            editorActions({
+                type: 'editor_select',
+                payload: props.id,
+            });
+        }
+    }
 
     const component = React.createElement(
         elementConfig[props.type as ElementType]?.experienceComponent ?? elementFallbackConfig.experienceComponent,
@@ -21,7 +42,7 @@ export function Element(props: ElementProps) {
     );
 
     return (
-        <Select enabled={selected}>
+        <Select enabled={selected} onDoubleClick={selectElement}>
             {component}
         </Select>
     );
