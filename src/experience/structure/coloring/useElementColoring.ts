bb30e272cff2fd9a1bf3e835a87e78be6b0daf3d
Kevin Lucas Simon <k.simon@basecom.de>
2025-11-22 21:28:25 +0100

    Update Coloring Hook

--- src/experience/structure/coloring/useElementColoring.ts
+++ src/experience/structure/coloring/useElementColoring.ts
@@ -3,19 +3,21 @@ import {useExperienceState} from "../../reducer/ExperienceProvider";
 import {StructureTypes} from "../../../data/model/structure/StructureTypes";
 import {StructureID} from "../../../data/model/structure/StructureModel";
 import {ColoringDefault, ColoringModel} from "../../../data/model/structure/material/ColoringModel";
+import {useMemo} from "react";
 
 export function useElementColoring(colorType: ColorType): ColorHex {
-    const level = useExperienceState().level;
-    const activeChunk = useExperienceState().activeChunkID;
+    const {level, activeChunkID} = useExperienceState();
 
-    const coloringStructures = Object.values(level.structures)
-        .filter(structure => structure.type === StructureTypes.Coloring)
+    const coloringStructures = useMemo(() => {
+        return Object.values(level.structures)
+            .filter(structure => structure.type === StructureTypes.Coloring)
+    }, [level.structures]);
 
     const getColoringHex = (parentID: StructureID | null): ColorHex => {
         const coloringStructure = coloringStructures.find(structure => structure.parent === parentID) as ColoringModel;
 
         if (coloringStructure && coloringStructure[colorType]) {
-            return coloringStructure[colorType] as ColorHex;
+            return coloringStructure[colorType];
         }
 
         const parentStructure = parentID ? level.structures[parentID] : null;
@@ -23,8 +25,8 @@ export function useElementColoring(colorType: ColorType): ColorHex {
             return getColoringHex(parentStructure.parent);
         }
 
-        return ColoringDefault[colorType] as ColorHex;
+        return ColoringDefault[colorType];
     };
 
-    return getColoringHex(activeChunk ? activeChunk : null);
+    return getColoringHex(activeChunkID);
 }
