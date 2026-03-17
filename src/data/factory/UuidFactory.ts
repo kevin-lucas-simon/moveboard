115e4cef3b7b15eb3f696553814d3d80439e2efb
Kevin Lucas Simon <kevin.simon@grow-agency.de>
2026-03-17 18:36:30 +0100

    Add typescript version

--- src/data/factory/UuidFactory.ts
+++ src/data/factory/UuidFactory.ts
@@ -1,5 +1,5 @@
-import {generateUUID} from "three/src/math/MathUtils";
 import {UUID} from "../model/UUID";
+import {generateUUID} from "three/src/math/MathUtils";
 
 export function createUUID(): UUID {
     return generateUUID() as UUID;
