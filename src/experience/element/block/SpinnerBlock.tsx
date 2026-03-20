91f13231acd0b760a6d571282381c5592cf32956
Kevin Lucas Simon <kevin.simon@grow-agency.de>
2026-03-20 12:09:16 +0100

    Refactor materials

--- src/experience/element/block/SpinnerBlock.tsx
+++ src/experience/element/block/SpinnerBlock.tsx
@@ -2,37 +2,11 @@ import {SpinnerBlockDefault, SpinnerBlockModel} from "../../../data/model/elemen
 import {RapierRigidBody, RigidBody} from "@react-three/rapier";
 import {useElementColoring} from "../../structure/coloring/useElementColoring";
 import {Vector3} from "three";
-import {useMemo, useRef} from "react";
+import {useRef} from "react";
 import {useFrame} from "@react-three/fiber";
 import {useSimulationSettings} from "../../debug/settings/SimulationSettingsProvider";
 import {Angle} from "../../../data/model/Angle";
-
-// @ts-ignore
-import { abs, add, color, fract, length, mrt, mul, normalView, output, pass, positionLocal, sin, step, sub, time } from 'three/tsl';
-
-const GlowingNodeMaterial = () => {
-    // useMemo ist extrem wichtig, damit der TSL-Graph nicht bei jedem
-    // React-Render-Zyklus neu aufgebaut wird!
-    const colorNode = useMemo(() => {
-        const _node0 = positionLocal;
-        const _node1 = time;
-        const _node2 = color("#0be941");
-        const _node3 = mul(_node0, 5);
-        const _node4 = fract(_node3);
-        const _node5 = sub(_node4, 0.5);
-        const _node6 = length(_node5);
-        const _node7 = mul(_node6, 10);
-        const _node8 = add(_node7, _node1);
-        const _node9 = sin(_node8);
-        const _node10 = abs(_node9);
-        const _node11 = step(0.5, _node10);
-
-        return mul(_node2, _node11);
-    }, []);
-
-    // @ts-ignore
-    return <meshStandardNodeMaterial colorNode={colorNode} />;
-};
+import {SwirlMaterial} from "../../material/SwirlMaterial";
 
 export function SpinnerBlock(props: SpinnerBlockModel = SpinnerBlockDefault) {
     const colorHex = useElementColoring(props.color);
@@ -60,8 +34,7 @@ export function SpinnerBlock(props: SpinnerBlockModel = SpinnerBlockDefault) {
             >
                 <mesh castShadow receiveShadow>
                     <boxGeometry args={new Vector3().copy(props.dimension).toArray()} />
-                    <GlowingNodeMaterial />
-                    {/*<meshStandardMaterial color={colorHex} />*/}
+                    <SwirlMaterial color={colorHex} />
                 </mesh>
             </RigidBody>
 
